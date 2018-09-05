97ad053706aa2c7533a7f6811c9189a87e172816
lpeltonjr <lpeltonjr@bellsouth.net>
2018-09-04 21:00:34 -0500

    corrected service worker scope after reviewing all conflicting online material related to whether scope is relative to website root or location of script that registers service worker; decided on latter even though Mozilla says former; finally finished debugging service worker; problems with missing "event.respondWith" and asynchronous JS in "activate" event listener

--- js/sworkercontrol.js
+++ js/sworkercontrol.js
@@ -2,10 +2,10 @@
 class sWorkerControl {
 
   //  pass in path to the JavaScript service worker and path defining its scope
-  constructor(workerPath, scopeStr) {
+  constructor(workerPath) {
     
     this.workerPath = workerPath;
-    this.scope = {scope: scopeStr};
+    
     //  ServiceWorkerRegistrationObject
     this.swRegObj = null;
     //  handle to a ServiceWorker instance
@@ -21,8 +21,9 @@ class sWorkerControl {
 
   //  create and display HTML form allowing user to select updating the site or ignoring us
   nagUserToUpdateSite() {
+    console.log(`nagUserToUpdateSite 01: ${this.workerPath}`);
     if (this.sWorker && (this.sWorker.state === 'installed')) {
-        
+        console.log(`nagUserToUpdateSite 02: state is installed`);    
         document.body.insertAdjacentHTML('afterbegin', `<div class="update-alert">
         <span>An update to this site is available.</span>
         <button class="install-sw">INSTALL</button>
@@ -45,6 +46,7 @@ class sWorkerControl {
 
   //  initialize event listener for new service worker attaining the "installed" state
   launchListenerForInstalled() {
+    console.log(`launchListenerForInstalled 01: ${this.workerPath}`);
     if (this.sWorker) {
         this.sWorker.addEventListener('statechange', this.nagUserToUpdateSite.bind(this));
     }
@@ -52,9 +54,10 @@ class sWorkerControl {
 
   //  event handler for ServiceWorkerRegistration.onupdatefound
   onUpdateFound() {
+    console.log(`onUpdateFound 01: ${this.workerPath}`);
     if (this.swRegObj) {
       //  the serviceWorker state is assumed to be "installing"; grab a handle on it
-      //  (or try), and if successful, lauch the event listener for its 'waiting' state
+      //  (or try), and if successful, launch the event listener for its 'waiting' state
       this.sWorker = this.swRegObj.installing;
       this.launchListenerForInstalled();
     }
@@ -63,9 +66,10 @@ class sWorkerControl {
   //    triggers chain of events that may result in removal of an outdated service worker and
   //    immediate launch of the new service worker
   prepOldWorkerRemoval(swRegObj) {
-    //console.log(this);
+    console.log(`prepOldWorkerRemoval 01: ${this.workerPath}`);
     //  latch reference to the ServiceWorkerRegistration object, so we can keep using it
     this.swRegObj = swRegObj;
+    console.log(`prepOldWorkerRemoval 02: ${this.swRegObj.scope}`);
 
     //  if there's an old service worker already active, set-up to determine
     //  when the new worker is finished installing so we can then delete the old
@@ -76,11 +80,14 @@ class sWorkerControl {
       //  as the service worker state
       //  case 1 -- see if it's installing; if so, set a listener to trigger when it's installed
       this.sWorker = swRegObj.installing;
-      if (this.sWorker) this.launchListenerForInstalled();
-      //  else new worker isn't installing, so we don't have a handle yet
-      else {
+      console.log(`prepOldWorkerRemoval: installing?: ${this.sWorker}`);
+      if (this.sWorker) {
+        this.launchListenerForInstalled();
+      } else {
+        //  else new worker isn't installing, so we don't have a handle yet
         //  case 2 -- if not installing, see if it's waiting already (which implies it's also "installed")
         this.sWorker = swRegObj.waiting;
+        console.log(`prepOldWorkerRemoval: waiting?: ${this.sWorker}`);
         //  no need for an event listener in case 2 -- just prompt user to update the site
         if (this.sWorker) {
           this.nagUserToUpdateSite();
@@ -90,26 +97,30 @@ class sWorkerControl {
         } else {
 
           this.swRegObj.onupdatefound = this.onUpdateFound.bind(this);
+          console.log(`prepOldWorkerRemoval: not installing or waiting`);
         }
       }
+    } else {
+      console.log(`prepOldWorkerRemoval: !navigator.serviceWorker.controller`);
     }    
   };
 
   //  call this after calling the constructor -- registers the service worker in the navigator
   registerWorker() {
+
+    console.log(`registerWorker 01: ${this.workerPath}`);
     
     //  if the browser supports the serviceWorker API
     if (navigator.serviceWorker) {
       //  register the service worker, setting its scope to the whole website, then set-up to prompt
       //  the user to update the website and launch the new service worker immediately
-      navigator.serviceWorker.register(this.workerPath, this.scope).then(this.prepOldWorkerRemoval.bind(this));
-      //navigator.serviceWorker.register("sw.js", {scope: './'}).then(this.prepOldWorkerRemoval.bind(this));
-      //console.log(document.URL);
+      navigator.serviceWorker.register(this.workerPath, {scope: '/'})
+      .then(this.prepOldWorkerRemoval.bind(this))
+      .catch(err=>{console.log(`registerWorker err: ${err}`);});
     }
   }
 
 }
 
-let SWcontroller = new sWorkerControl('sw.js', './');
+let SWcontroller = new sWorkerControl('../sw.js');
 SWcontroller.registerWorker();
-//SWcontroller.nagUserToUpdateSite();
--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ const assets = [
     './',
     './index.html',
     './restaurant.html',
-//  'sw.js',
     './js/',
     './js/dbhelper.js',
     './js/main.js',
@@ -12,50 +11,78 @@ const assets = [
     './data/',
     './data/restaurants.json',
     './css/',
-    './css/styles.css'
+    './css/styles.css'/*,
+    './img/1_44.jpg',
+    './img/1_66.jpg',
+    './img/1.jpg',
+    './img/2_44.jpg',
+    './img/2_66.jpg',
+    './img/2.jpg',
+    './img/3_44.jpg',
+    './img/3_66.jpg',
+    './img/3.jpg',
+    './img/4_44.jpg',
+    './img/4_66.jpg',
+    './img/4.jpg',     
+    './img/5_44.jpg',
+    './img/5_66.jpg',
+    './img/5.jpg',  
+    './img/6_44.jpg',
+    './img/6_66.jpg',
+    './img/6.jpg',   
+    './img/7_44.jpg',
+    './img/7_66.jpg',
+    './img/7.jpg', 
+    './img/8_44.jpg',
+    './img/8_66.jpg',
+    './img/8.jpg',    
+    './img/9_44.jpg',
+    './img/9_66.jpg',
+    './img/9.jpg',
+    './img/10_44.jpg',
+    './img/10_66.jpg',
+    './img/10.jpg' */  
 ];
 
 //  cache most assets on install
 self.addEventListener('install', (event)=>{
-    event.waitUntil(caches.open(myCache).then(
-        cache=>cache.addAll(assets)
-    ));
+    console.log(`enter sw install event handler`);    
+    event.waitUntil(
+        caches.open(myCache)
+        .then(cache=>cache.addAll(assets))
+//      .then(()=>console.log(`initial cache complete`))
+        .catch(err=>console.log(`install event handler error: ${err}`))
+    );
 });
+
 
 //  intercept fetch events and supply reponse from cache, if possible
 self.addEventListener('fetch', (event)=>{
-    caches.open(myCache).then(
-        (cache)=>{
-            cache.match(event.request, {ignoreSearch: true, ignoreVary: true}).then(
-                (response)=>{
-                    if (response) return(response);
-                    //  if response can't be supplied from cache, get it off the
-                    //  web and cache it (if possible) before supplying it to
-                    //  the site, so it can be supplied from the cache next time
-                    fetch(event.request).then(
-                        (resp)=>{
-                            //  note that this restaurant review site has some assets that
-                            //  throw an error at this point when cache is attempted;
-                            //  something to do with http and chrome-extension;
-                            //  I don't know why, though there are technical explanations for it
-                            //  online; I'm just trying to ignore the error without allowing it to 
-                            //  cause problems
-                            cache.put(event.request, resp.clone()).then(
-                                ()=>resp
-                            ).catch(
-                                (e)=>resp
-                            );
-                        }
-                    );
-                }
-            ).catch(
-                (error)=>{
-                    console.log(error);
-                }
-            );
-        }
+    event.respondWith(
+        caches.open(myCache)
+        .then(cache=>cache.match(event.request))
+        .then(response=>{
+            //  if the request/response couldn't be located in the cached fetch
+            //  events, then go ahead and fetch it online and then cache it for next time
+            if (!response) {
+                return (
+                    fetch(event.request)
+                    .then(response=>{
+                        caches.open(myCache)
+                        .then(cache=>cache.put(event.request, response.clone()));
+                        return (response);
+                    })
+                );
+            //  but if the request/response were located in the cache, supply the cached response
+            //  to the browser
+            }   else {
+                return (response);
+            }
+        })
     );
 });
+
+
 
 //  when the SWcontroller instance sends us a message to skip waiting and takeover
 self.addEventListener('message', (event)=>{
@@ -67,29 +94,33 @@ self.addEventListener('message', (event)=>{
 
 //  when this service worker first activates ...
 self.addEventListener('activate', (event)=>{
-
-    //  get the list of caches in the cache storage
-    event.waitUntil(caches.keys().then(
-        (list)=>{
+    event.waitUntil(
+        caches.keys()
+        .then((list)=>{
             //  the cache we implemented above is also included, so first remove it
             //  from the list
             let idx1 = list.indexOf(myCache);
+            console.log(`activate event listener: index of myCache: ${idx1}`);
             let promiseList = [];
             if (idx1 !== -1) {
                 list.splice(idx1, 1);
+                console.log(`activate event listener: list size after splice: ${list.length}`);
                 //  after removing the new cache from the list, delete all old versions
                 //  of the new cache, from old service workers
                 list.forEach((item)=>{
-                    if (item.startsWith('mySiteVersion'))
-                    {
+                    if (item.startsWith('mySiteVersion'))   {
+                        console.log(`activate event listener: deleting ${item} from caches`);
                         promiseList.push(caches.delete(item));
                     }
                 });
-        
-                return(Promise.all(promiseList));
             }
-        }
-    ).then(
-        ()=>self.clients.claim()
-    ));
+            return(Promise.all(promiseList));
+        })
+        .then(()=>{
+            console.log(`activate event listener: finished deleting caches`);
+        })
+        .catch(err=>{
+            console.log(`activate event listener: final error: ${err}`);
+        })
+    );
 });
