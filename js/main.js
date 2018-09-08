8f8267779e90a203a55b2259708227fac7e1f739
lpeltonjr <lpeltonjr@bellsouth.net>
2018-09-08 18:17:38 -0500

    changed Arial label for View Details buttons

--- js/main.js
+++ js/main.js
@@ -180,7 +180,7 @@ createRestaurantHTML = (restaurant) => {
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
   more.href = DBHelper.urlForRestaurant(restaurant);
-  more.setAttribute("aria-label", `view details about ${restaurant.name}`);
+  more.setAttribute("aria-label", `more details about ${restaurant.name}`);
   li.append(more)
 
   return li
